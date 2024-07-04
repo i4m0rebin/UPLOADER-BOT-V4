4488f100bd4efcc54f714251fda8d6a51d6f6ecc
Rebin Coder <167017517+i4m0rebin@users.noreply.github.com>
2024-07-04 13:57:51 +0300

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -47,7 +47,7 @@ class Config(object):
     
     LOGGER = logging
 
-    #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001319856754")
     
     OWNER_ID = int(os.environ.get("OWNER_ID", "1150866732"))
     
